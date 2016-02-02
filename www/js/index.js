a2d0af38594bb102d848e5a476ed67ecc4595814
Kuswandanu Kusuma Wicaksana <kuswandanu@users.noreply.github.com>
2016-02-02 21:35:15 +0700

    Mulai ajar iklan

--- www/js/index.js
+++ www/js/index.js
@@ -8,12 +8,23 @@ var app = {
     onDeviceReady : function() {
         alert("PhoneGap is working");
         document.addEventListener("backbutton", app.onBackKeyDown, false);
+
+        admob.setOptions({
+            publisherID: 'ca-app-pub-9863325511078756/9802347428',
+            bannerAtTop: false, // set to true, to put banner at top
+            overlap: false, // set to true, to allow banner overlap webview
+            offsetStatusBar: true, // set to true to avoid ios7 status bar overlap
+            isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
+            autoShowBanner: true, // auto show banners ad when loaded
+            autoShowInterstitial: false // auto show interstitials ad when loaded
+        });
+        admob.createBannerView();
     },
 
     onBackKeyDown : function() {
         if(app.inMenu == "")
-            // navigator.app.exitApp();
-            alert("keluar");
+            navigator.app.exitApp();
+            // alert("keluar");
         else
         {
             removeClass(document.getElementById(app.inMenu), "visible");
